a7ea2fd7c524c60ba1ca8d4b0b8de50d028472d8
a-wing <1@233.email>
2020-06-27 14:19:35 +0800

    Add (jsonrpc2mqtt): simulation

--- jsonrpc2mqtt/proxy_test.go
+++ jsonrpc2mqtt/proxy_test.go
@@ -1,101 +1,11 @@
 package jsonrpc2mqtt
 
 import (
-	"errors"
 	"testing"
-	"time"
-
-	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
-type MockToken struct{}
-
-func (t *MockToken) Wait() bool {
-	return true
-}
-
-func (t *MockToken) WaitTimeout(time time.Duration) bool {
-	return true
-}
-
-func (t *MockToken) Error() error {
-	return errors.New("")
-}
-
-type mqttClient struct {
-	PublishData       []byte
-	SubscribeCallback mqtt.MessageHandler
-}
-
-func (m *mqttClient) IsConnected() bool {
-	return true
-}
-
-func (m *mqttClient) IsConnectionOpen() bool {
-	return true
-}
-
-func (m *mqttClient) Connect() mqtt.Token {
-	return &MockToken{}
-}
-
-func (m *mqttClient) Disconnect(quiesce uint) {}
-
-func (m *mqttClient) Publish(topic string, qos byte, retained bool, payload interface{}) mqtt.Token {
-	m.PublishData = payload.([]byte)
-	return &MockToken{}
-}
-
-func (m *mqttClient) Subscribe(topic string, qos byte, callback mqtt.MessageHandler) mqtt.Token {
-	m.SubscribeCallback = callback
-	return &MockToken{}
-}
-
-func (m *mqttClient) SubscribeMultiple(filters map[string]byte, callback mqtt.MessageHandler) mqtt.Token {
-	return &MockToken{}
-}
-func (m *mqttClient) Unsubscribe(topics ...string) mqtt.Token {
-	return &MockToken{}
-}
-func (m *mqttClient) AddRoute(topic string, callback mqtt.MessageHandler) {}
-
-func (m *mqttClient) OptionsReader() mqtt.ClientOptionsReader {
-	return mqtt.ClientOptionsReader{}
-}
-
-type MockMessage struct {
-	DataTopic   string
-	DataPayload []byte
-}
-
-func (m *MockMessage) Duplicate() bool {
-	return true
-}
-
-func (m *MockMessage) Qos() byte {
-	return byte(0)
-}
-
-func (m *MockMessage) Retained() bool {
-	return true
-}
-
-func (m *MockMessage) Topic() string {
-	return m.DataTopic
-}
-
-func (m *MockMessage) MessageID() uint16 {
-	return 233
-}
-
-func (m *MockMessage) Payload() []byte {
-	return m.DataPayload
-}
-
-func (m *MockMessage) Ack() {}
-
 func Test_mqttproxy(t *testing.T) {
-	client := &mqttClient{}
+	client := &MockClient{}
 
 	mqttProxy, _ := OpenMqttProxy(client)
 	mqttProxy.Notify("233", []byte("2222222222233333333333333"))
