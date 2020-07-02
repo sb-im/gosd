b9a2dece6035ac04e342aa77ef86aca341875466
a-wing <1@233.email>
2020-07-02 17:17:34 +0800

    Add (api): AuthMiddleware

--- api/api.go
+++ api/api.go
@@ -23,6 +23,7 @@ func Serve(router *mux.Router, store *storage.Storage, worker *luavm.Worker, bas
 	//sr.Use(middleware.apiKeyAuth)
 
 	sr.Use(CORSOriginMiddleware("*"))
+	sr.Use(handler.AuthMiddleware)
 
 	//router.Use(mux.CORSMethodMiddleware(sr))
 	router.HandleFunc(u.Path+"/oauth/token", handler.authHandler).Methods(http.MethodPost, http.MethodOptions)
--- api/middleware.go
+++ api/middleware.go
@@ -2,6 +2,9 @@ package api
 
 import (
 	"net/http"
+	"strings"
+
+	"miniflux.app/http/response/json"
 )
 
 func CORSOriginMiddleware(origin string) func(http.Handler) http.Handler {
@@ -11,4 +14,19 @@ func CORSOriginMiddleware(origin string) func(http.Handler) http.Handler {
 			next.ServeHTTP(w, req)
 		})
 	}
+}
+
+func (h handler) AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+
+		key := strings.Split(req.Header.Get("Authorization"), " ")[1]
+		user := h.store.GetCurrentUser(key)
+
+		if user == nil {
+			json.Unauthorized(w, req)
+			return
+		}
+
+		next.ServeHTTP(w, req)
+	})
 }
