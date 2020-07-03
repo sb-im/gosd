202ae57f3e1630e1ddaf05a5c7237e164f987f69
a-wing <1@233.email>
2020-07-03 15:35:48 +0800

    Change (api): node belong group

--- api/action.go
+++ api/action.go
@@ -1,14 +1,30 @@
 package api
 
 import (
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 
 	"miniflux.app/http/request"
 	"miniflux.app/http/response/json"
 )
 
 func (h *handler) actionHandler(w http.ResponseWriter, r *http.Request) {
+	user := h.helpCurrentUser(w, r)
+	if user != nil {
+		content, err := ioutil.ReadFile("data/" + strconv.FormatInt(user.Group.ID, 10) + "/" + request.RouteStringParam(r, "action") + ".json")
+
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(content)
+			return
+		}
+
+	}
+
 	content, err := ioutil.ReadFile("data/" + request.RouteStringParam(r, "action") + ".json")
 	if err != nil {
 		json.ServerError(w, r, err)
