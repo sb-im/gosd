f1dba758c49f58fe65777f4f607f8180bdec1826
a-wing <1@233.email>
2020-07-10 21:26:56 +0800

    Fix (api): create planlog not from-data error

--- api/planlog.go
+++ api/planlog.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	"fmt"
 	"io/ioutil"
 	"mime"
 	"net/http"
@@ -20,17 +19,11 @@ func (h *handler) missionQueue(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *handler) createPlanLog(w http.ResponseWriter, r *http.Request) {
-	mediaType, _, err := mime.ParseMediaType(r.Header.Get("Content-Type"))
-	if err != nil {
-		json.ServerError(w, r, err)
-		return
-	}
-
 	log := model.NewPlanLog()
 	//log.LogID = request.RouteInt64Param(r, "logID")
 	log.PlanID = request.RouteInt64Param(r, "planID")
 
-	if strings.HasPrefix(mediaType, "multipart/") {
+	if mediaType, _, err := mime.ParseMediaType(r.Header.Get("Content-Type")); err == nil && strings.HasPrefix(mediaType, "multipart/") {
 
 		params, file, err := h.formData2Blob(r)
 		if err != nil {
@@ -40,9 +33,6 @@ func (h *handler) createPlanLog(w http.ResponseWriter, r *http.Request) {
 
 		log.Attachments = file
 		log.Extra = params
-
-	} else {
-		fmt.Println("Not Form Data")
 	}
 
 	if err := h.store.CreatePlanLog(log); err != nil {
