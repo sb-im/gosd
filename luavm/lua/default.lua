3c2fa2a264da29473220df245d8bd2e8e36490e8
a-wing <1@233.email>
2020-07-11 13:02:45 +0800

    Change (luavm): default.lua

--- luavm/lua/default.lua
+++ luavm/lua/default.lua
@@ -3,59 +3,130 @@ local json = require("json")
 function run(node_id)
   print("=== START Lua ===")
 
-  print(get_id("link_id"))
+  local drone_id = node_id
+  local depot_id =get_id("link_id")
+
   print(json.encode(get_status()))
 
   print(node_id)
 
-  local data, err = get_msg("8", "weather")
-  print(json.encode(data))
+  --local data, err = get_msg("8", "weather")
+  --print(json.encode(data))
 
-  local err = rpc_notify(node_id, {
-    ["method"] = "test",
-    ["params"] = {
-      ["a"] = "233",
-      ["b"] = "456"
-    }
+  local data = rpc_call(depot_id, {
+    ["method"] = "power_chargedrone_on",
   })
 
-  print("=============")
-  if err ~= ""
-    then
-      print(err)
-    end
-  print("=============")
+  if data["result"] then
+    print("power_chargedrone_on success")
+  else
+    print("EEEEEEEEEEE")
+    print(json.encode(data))
+    return
+  end
 
-  ch = channel.make()
-  rpc_async(node_id, {
-    ["method"] = "test",
-    ["params"] = {
-      ["a"] = "233",
-      ["b"] = "456"
-    }
-  }, ch)
+  ch_onDrone = channel.make()
+  rpc_async(drone_id, {
+    ["method"] = "power_on_drone",
+  }, ch_onDrone)
+
+  ch_onDoor = channel.make()
+  rpc_async(depot_id, {
+    ["method"] = "dooropen",
+  }, ch_onDoor)
+
+
   print("asyncCall send")
 
+  ch_onFree = channel.make()
+  ch_onLoad = channel.make()
   local res = {}
-  channel.select(
-  {"|<-", ch, function(ok, data)
-    print(ok, data)
-    print(json.encode(data))
-    if data["result"]
-      then
-        print("asyncCall success")
-      end
-  end}
-  )
-  print(res["result"])
 
-  print(json.encode(rpc_call(node_id, {
-    ["method"] = "sync1"
-  })))
-  print(json.encode(rpc_call(node_id, {
-    ["method"] = "sync2"
-  })))
+  err = 10
+  step_1 = 0
+  while( step_1 < 2 ) do
+    channel.select(
+    {"|<-", ch_onDrone, function(ok, data)
+      if data["result"] then
+        print("Drone Battery started")
+
+        rpc_async(drone_id, {
+          ["method"] = "loadmap",
+        }, ch_onLoad)
+      else
+        print("EEEEEEEEEEE")
+        print(json.encode(data))
+        step_1 = err
+      end
+    end},
+    {"|<-", ch_onLoad, function(ok, data)
+      if data["result"] then
+        print("Load Map")
+        step_1 = step_1 + 1
+      else
+        print("EEEEEEEEEEE")
+        print(json.encode(data))
+        step_1 = err
+      end
+    end},
+    {"|<-", ch_onDoor, function(ok, data)
+      if data["result"] then
+        rpc_async(depot_id, {
+          ["method"] = "freedrone",
+        }, ch_onFree)
+      else
+        print("EEEEEEEEEEE")
+        print(json.encode(data))
+        step_1 = err
+      end
+    end},
+    {"|<-", ch_onFree, function(ok, data)
+      if data["result"] then
+        local data = rpc_call(depot_id, {
+          ["method"] = "check_ready",
+        })
+        if data["result"] then
+          print("check_ready success")
+          step_1 = step_1 + 1
+        else
+          print("EEEEEEEEEEE")
+          print(json.encode(data))
+          step_1 = err
+        end
+      else
+        print("EEEEEEEEEEE")
+        print(json.encode(data))
+        step_1 = err
+      end
+    end})
+  end
+
+
+  if step_1 >= 10 then
+    return
+  end
+
+  local data = rpc_call(drone_id, {
+    ["method"] = "startmission_ready",
+  })
+
+  if not data["result"] then
+    print("EEEEEEEEEEE")
+    print(json.encode(data))
+    return
+  end
+
+
+  local data = rpc_call(drone_id, {
+    ["method"] = "startmission",
+  })
+
+  if not data["result"] then
+    print("EEEEEEEEEEE")
+    print(json.encode(data))
+    return
+  end
 
   print("=== END Lua END ===")
-  return res['data']
+  return
 end
