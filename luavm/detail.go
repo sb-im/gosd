851a50291822b725a9ce4539caf03f5ee1da3b61
a-wing <1@233.email>
2020-09-15 18:20:45 +0800

    Add (luavm): mqtt plan.status

--- luavm/detail.go
+++ luavm/detail.go
@@ -1,10 +1,19 @@
 package luavm
 
-import mqtt "github.com/eclipse/paho.mqtt.golang"
+import (
+	"encoding/json"
+	"fmt"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
 
 const (
-	StatusReady = "ready"
-	StatusError = "error"
+	topic_detail = "plans/%s/status"
+)
+
+const (
+	StatusReady   = "ready"
+	StatusError   = "error"
 	StatusProtect = "protect"
 	StatusRunning = "running"
 )
@@ -14,25 +23,32 @@ type Detail struct {
 }
 
 type StatusManager struct {
-	Plans map[string]*Detail
-	Client *mqtt.Client
+	Plans  map[string]*Detail
+	Client mqtt.Client
 }
 
-func NewStatusManager(client *mqtt.Client) *StatusManager {
+func NewStatusManager(client mqtt.Client) *StatusManager {
 	return &StatusManager{
-		Plans: map[string]*Detail{},
+		Plans:  map[string]*Detail{},
 		Client: client,
 	}
 }
 
-func (s *StatusManager) SetStatus(planID, status string) {
-	if detail := s.Plans[planID]; detail == nil {
+func (s *StatusManager) SetStatus(planID, status string) error {
+	detail := s.Plans[planID]
+	if detail == nil {
 		detail = &Detail{
 			Status: status,
 		}
 	} else {
 		detail.Status = status
 	}
+	data, err := json.Marshal(detail)
+	if err != nil {
+		return err
+	}
+
+	return s.Client.Publish(fmt.Sprintf(topic_detail, planID), 1, true, data).Error()
 }
 
 func (s *StatusManager) GetStatus(planID string) string {
@@ -42,4 +58,3 @@ func (s *StatusManager) GetStatus(planID string) string {
 		return s.Plans[planID].Status
 	}
 }
-
