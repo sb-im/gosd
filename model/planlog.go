5d03986c33cfd22ebb2470a72578edcf5eb98670
a-wing <1@233.email>
2020-09-25 18:31:53 +0800

    Change (api): log_id => job_id

--- model/planlog.go
+++ model/planlog.go
@@ -2,7 +2,7 @@ package model
 
 type PlanLog struct {
 	ID          int64             `json:"id"`
-	LogID       int64             `json:"log_id"`
+	LogID       int64             `json:"job_id"`
 	PlanID      int64             `json:"plan_id"`
 	Attachments map[string]string `json:"files"`
 	Extra       map[string]string `json:"extra"`
