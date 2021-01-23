856f21c5c8feb310195b850f71fea810b2fad19e
a-wing <1@233.email>
2021-01-23 19:36:21 +0800

    Fix (api/v1/blob): downloadBlob filename garbled
    Fix Chinese filename garbled
    Fix https://gitlab.com/sbim/superdock/cloud/sdwc/-/issues/123

--- api/blob.go
+++ api/blob.go
@@ -19,7 +19,11 @@ import (
 
 func handleDownload(filename string, reader io.Reader, w http.ResponseWriter) {
 	w.Header().Add("Content-type", "application/octet-stream")
-	w.Header().Add("content-disposition", "attachment; filename=\""+filename+"\"")
+
+	// https://gitlab.com/sbim/superdock/cloud/sdwc/-/issues/123
+	// https://blog.csdn.net/liuyaqi1993/article/details/78275396
+	//w.Header().Add("content-disposition", "attachment; filename=\""+filename+"\"")
+	w.Header().Add("content-disposition", `attachment; filename* = UTF-8''`+filename)
 	_, err := io.Copy(w, reader)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
