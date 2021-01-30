b01e767dd511ca59935680611ad9f3741b7a44f0
a-wing <1@233.email>
2021-01-30 16:45:19 +0800

    Improve (luavm): lua

--- luavm/lua/test_rpc.lua
+++ luavm/lua/test_rpc.lua
@@ -5,23 +5,9 @@ function main(plan)
   local drone_id = plan.nodeID
   local drone = NewNode(drone_id)
   xpcall(function()
-    drone:SyncCall("mode_brake")
+    drone:SyncCall("get")
   end,
   function()
     print(debug.traceback())
   end)
-
-  -- local depot_id = drone:GetID()
-  -- local depot = NewNode(depot_id)
-
-  -- local plan = NewPlan()
-
-  -- print("Drone Id:", drone.id)
-  -- print("Depot Id:", depot.id)
-
-  -- print("Drone Status:", json.encode(drone:GetStatus()))
-  -- local ok, data = pcall(drone.GetMsg, drone, "battery")
-  -- if ok then
-  --   print(json.encode(data))
-  -- end
 end
