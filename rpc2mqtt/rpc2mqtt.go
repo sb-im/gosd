8bd9242234b6f92d04cbbf4a32d46d2150a9a333
a-wing <1@233.email>
2021-03-31 12:52:05 +0800

    Change (rpc2mqtt): jsonrpc resend

--- rpc2mqtt/rpc2mqtt.go
+++ rpc2mqtt/rpc2mqtt.go
@@ -3,15 +3,26 @@ package rpc2mqtt
 import (
 	"context"
 	"errors"
+	"fmt"
+	"math"
 	"sync"
+	"time"
 
 	"sb.im/gosd/mqttd"
 
 	"github.com/sb-im/jsonrpc-lite"
 )
 
+type Pending struct {
+	Dst   string
+	Msg   []byte
+	Time  time.Time
+	Reply chan []byte
+	Count int
+}
+
 type Rpc2mqtt struct {
-	pending map[jsonrpc.ID]chan []byte
+	pending map[jsonrpc.ID]*Pending
 	mutex   sync.Mutex
 	i       chan<- mqttd.MqttRpc
 	o       <-chan mqttd.MqttRpc
@@ -21,7 +32,7 @@ func NewRpc2Mqtt(i chan<- mqttd.MqttRpc, o <-chan mqttd.MqttRpc) *Rpc2mqtt {
 	return &Rpc2mqtt{
 		i:       i,
 		o:       o,
-		pending: make(map[jsonrpc.ID]chan []byte),
+		pending: make(map[jsonrpc.ID]*Pending),
 	}
 }
 
@@ -32,7 +43,13 @@ func (t *Rpc2mqtt) AsyncRpc(id string, req []byte, ch_res chan []byte) error {
 		Payload: req,
 	}:
 		t.mutex.Lock()
-		t.pending[*jsonrpc.ParseObject(req).ID] = ch_res
+		t.pending[*jsonrpc.ParseObject(req).ID] = &Pending{
+			Time:  time.Now(),
+			Dst:   id,
+			Msg:   req,
+			Count: 1,
+			Reply: ch_res,
+		}
 		t.mutex.Unlock()
 	default:
 		return errors.New("Buffer full")
@@ -41,6 +58,7 @@ func (t *Rpc2mqtt) AsyncRpc(id string, req []byte, ch_res chan []byte) error {
 }
 
 func (t *Rpc2mqtt) Run(ctx context.Context) {
+	go t.Resend(ctx)
 	for {
 		select {
 		case <-ctx.Done():
@@ -50,12 +68,46 @@ func (t *Rpc2mqtt) Run(ctx context.Context) {
 			if rpc.Type == jsonrpc.TypeInvalid {
 				continue
 			}
-			if pending := t.pending[*rpc.ID]; pending != nil && (rpc.Type == jsonrpc.TypeSuccess || rpc.Type == jsonrpc.TypeErrors) {
+			if pending, ok := t.pending[*rpc.ID]; ok && (rpc.Type == jsonrpc.TypeSuccess || rpc.Type == jsonrpc.TypeErrors) {
 				t.mutex.Lock()
 				delete(t.pending, *rpc.ID)
 				t.mutex.Unlock()
-				pending <- raw.Payload
+				pending.Reply <- raw.Payload
 			}
+		}
+	}
+}
+
+func (t *Rpc2mqtt) Resend(ctx context.Context) {
+	// Max Resend interval 1h
+	timeout := float64(3600)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			for id, pending := range t.pending {
+				if now := time.Since(pending.Time).Seconds(); now > math.Ldexp(1, pending.Count) && now <= timeout {
+					select {
+					case t.i <- mqttd.MqttRpc{
+						ID:      pending.Dst,
+						Payload: pending.Msg,
+					}:
+						pending.Count++
+
+					default:
+						fmt.Println("Buffer full")
+					}
+				} else if now > timeout {
+					rpc := jsonrpc.NewError(id, 1, "timeout", nil)
+					data, _ := rpc.ToJSON()
+					t.mutex.Lock()
+					delete(t.pending, *rpc.ID)
+					t.mutex.Unlock()
+					pending.Reply <- data
+				}
+			}
+			time.Sleep(1 * time.Second)
 		}
 	}
 }
