2cf1daddda58ad79fd057ca52cde842c284031f3
a-wing <1@233.email>
2021-05-15 14:47:15 +0800

    Add (luavm): NewLog fnWord

--- luavm/lua/lib_log.lua
+++ luavm/lua/lib_log.lua
@@ -4,23 +4,32 @@
     return tostring(nu) .. ": \t" .. os.date("%Y/%m/%d %H:%M:%S") .. " " .. line
   end)
 --]]
-function NewLog(fn)
-  if fn == nil then
-    fn = function(line, nu)
+function NewLog(fnLine, fnWord)
+
+  if fnLine == nil then
+    fnLine = function(line, nu)
       return tostring(nu) .. ": " .. line
     end
   end
+
+  if fnWord == nil then
+    fnWord = function(word, nu)
+      return tostring(word)
+    end
+  end
+
   return {
     _nu = 0,
-    _call = fn,
     _content = "",
+    _fnLine = fnLine,
+    _fnWord = fnWord,
     Println = function(self, ...)
       self._nu = self._nu + 1
       local line = ""
       for i, v in ipairs(arg) do
-        line = line .. tostring(v) .. "\t"
+        line = line .. self._fnWord(v, i) .. "\t"
       end
-      self._content = self._content .. self._call(line, self._nu) .. "\n"
+      self._content = self._content .. self._fnLine(line, self._nu) .. "\n"
     end,
     GetContent = function(self)
       return self._content
@@ -49,7 +58,7 @@ function test_log()
   print(log:GetContent())
 end
 
-function test_logfn()
+function test_logFnLine()
   local log = NewLog(function(line, nu)
     return tostring(nu) .. ": \t" .. os.date("%Y/%m/%d %H:%M:%S") .. " " .. line
   end)
@@ -61,7 +70,23 @@ function test_logfn()
   print(log:GetContent())
 end
 
+function test_logFnWord()
+  local log = NewLog(function(line, nu)
+    return tostring(nu) .. ": \t" .. os.date("%Y/%m/%d %H:%M:%S") .. " " .. line
+  end,
+  function(word, nu)
+    return tostring(nu) .. ": " .. word
+  end)
+  log:Println("xxxxx")
+  log:Println("xxxxx", "xxxxx")
+  log:Println("xxxxx", "xxxxx", "xxxxx")
+  log:Println("xxxxx", "xxxxx", "xxxxx", "xxxxx")
+  log:Println(1, "xxxxx")
+  print(log:GetContent())
+end
+
 test_log()
-test_logfn()
+test_logFnLine()
+test_logFnWord()
 --]]
 
