3f4ee6d932305c9be167dabac5ee58c16e944045
a-wing <1@233.email>
2022-01-10 23:41:06 +0800

    feat(app/v3/schedule): add schedule redis

--- app/service/schedule.go
+++ app/service/schedule.go
@@ -1,22 +1,60 @@
 package service
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/robfig/cron/v3"
 	"sb.im/gosd/app/model"
 )
 
 func (s Service) StartSchedule() {
 	schedules := []model.Schedule{}
-	s.orm.Find(&schedules)
+	s.orm.Find(&schedules, "enable = TRUE")
 
 	for _, i := range schedules {
-		s.cron.AddFunc(i.Cron, func() {
-			s.JSON.Call(i.Method, []byte(i.Params))
-		})
+		s.ScheduleAdd(i)
 	}
 
 	s.cron.Start()
+}
+
+func (s Service) scheduleEntrySet(id uint, entryID int) {
+	s.rdb.Set(context.Background(), fmt.Sprintf("schedule.%d", id), entryID, 0)
+}
+
+func (s Service) scheduleEntryGet(id uint) (int, error) {
+	return s.rdb.Get(context.Background(), fmt.Sprintf("schedule.%d", id)).Int()
+}
+
+func (s Service) scheduleEntryDel(id uint) {
+	s.rdb.Del(context.Background(), fmt.Sprintf("schedule.%d", id))
+}
+
+func (s Service) ScheduleAdd(schedule model.Schedule) {
+	if entryID, err := s.cron.AddFunc(schedule.Cron, func() {
+		s.JSON.Call(schedule.Method, []byte(schedule.Params))
+	}); err != nil {
+		fmt.Println(err)
+	} else {
+		s.scheduleEntrySet(schedule.ID, int(entryID))
+	}
+}
+
+func (s Service) ScheduleDel(schedule model.Schedule) {
+	if entryID, err := s.scheduleEntryGet(schedule.ID); err != nil {
+		fmt.Println(err)
+	} else {
+		s.cron.Remove(cron.EntryID(entryID))
+		s.scheduleEntryDel(schedule.ID)
+	}
+}
+
+func (s Service) ScheduleUpdate(schedule model.Schedule) {
+	s.ScheduleDel(schedule)
+	if schedule.Enable {
+		s.ScheduleAdd(schedule)
+	}
 }
 
 func (s *Service) AddSchedule(task *model.Schedule) error {
