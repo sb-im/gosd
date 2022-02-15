ef6de4810dbd4ffa42c4626e7306a8d526ebd17c
a-wing <1@233.email>
2022-02-16 00:02:05 +0800

    fix(luavm): lib geo
    fix #69
    https://pkg.go.dev/math#Acos

--- luavm/lua/lib_geo.lua
+++ luavm/lua/lib_geo.lua
@@ -14,14 +14,30 @@ Geo = {}
 --]]
 
 function Geo:Distance(aLng, aLat, bLng, bLat)
+  local range = function(x)
+    -- https://pkg.go.dev/math#Acos
+    -- Acos(x) = NaN if x < -1 or x > 1
+    local min = -1
+    local max = 1
+    if x > max then
+      return max
+    elseif x < min then
+      return min
+    else
+      return x
+    end
+  end
+
   -- Earth Radius: 6371004
   return 6371004 * math.acos(
+    range(
       math.sin(math.rad(aLat)) * math.sin(math.rad(bLat))
       +
       math.cos(math.rad(aLat)) * math.cos(math.rad(bLat))
       *
       math.cos(math.rad(bLng - aLng))
     )
+  )
 end
 
 function GetDistance(aLng, aLat, bLng, bLat)
--- luavm/lua/test_geo.lua
+++ luavm/lua/test_geo.lua
@@ -18,5 +18,20 @@ function main(plan)
     error("Distance sum error:", distance)
   end
 
+  -- 118.7476783, 31.9414349
+  -- 118.7476783, 31.9414349
+  -- = 0
+  --
+  -- Maybe == NaN
+  -- math.acos(0.2798961794852986 + 0.7201038205147016)
+  -- https://pkg.go.dev/math#Acos
+  -- Acos(x) = NaN if x < -1 or x > 1
+  local distance_nan = GetDistance(118.7476783, 31.9414349, 118.7476783, 31.9414349)
+  if math.floor(distance_nan) == 0 then
+    print("Distance:", distance_nan)
+  else
+    error("Distance sum error:", distance_nan)
+  end
+
   print("=== END Geography END ===")
 end
