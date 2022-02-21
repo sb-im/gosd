2125d3e9ecf2e29bbefbbf10c66bd180c6774795
a-wing <1@233.email>
2022-02-21 22:07:30 +0800

    fix(api/v3): create task panic

--- app/model/task.go
+++ app/model/task.go
@@ -6,7 +6,7 @@ type Task struct {
 	Name   string `json:"name" form:"name"`
 	TeamID uint   `json:"-"`
 	NodeID uint   `json:"node_id" form:"node_id"`
-	Job    *Job   `json:"job,omitempty"`
+	Job    *Job   `json:"job,omitempty" form:"-"`
 	Files  JSON   `json:"files"`
 	Extra  JSON   `json:"extra"`
 }
