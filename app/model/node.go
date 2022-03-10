1552c691b0d8154c2158f524ac7add9ddaa437b5
a-wing <1@233.email>
2022-03-11 00:12:27 +0800

    fix(api/v3): node create params default []

--- app/model/node.go
+++ app/model/node.go
@@ -1,9 +1,18 @@
 package model
 
+import "gorm.io/gorm"
+
 type Node struct {
 	Model
 
 	Name   string `json:"name" form:"name"`
 	TeamID uint   `json:"-"`
 	Points JSON   `json:"points"`
+}
+
+func (n *Node) BeforeSave(tx *gorm.DB) error {
+	if !jsonIsArray(n.Points) {
+		n.Points = JSON("[]")
+	}
+	return nil
 }
