e257dd33320092a012b2fe358ada8576bd21433f
a-wing <1@233.email>
2022-03-11 00:15:10 +0800

    feat(cmd): add node ls points

--- app/cmd/node_ls.go
+++ app/cmd/node_ls.go
@@ -1,8 +1,10 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 	"text/tabwriter"
 
 	"github.com/urfave/cli/v2"
@@ -16,17 +18,33 @@ var nodeLsCmd = &cli.Command{
 	Name:  "ls",
 	Usage: "Ls all node",
 	Action: ex(func(c *exContext) error {
+		type point struct {
+			NodeID int             `json:"node_id"`
+			Params json.RawMessage `json:"params"`
+			Type   string          `json:"type"`
+		}
 		nodes, err := c.cnt.NodeIndex()
 		if err != nil {
 			return err
 		}
 		w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
-		fmt.Fprintln(w, "ID\tName")
+		fmt.Fprintln(w, "ID\tName\tPoints")
 
 		for _, i := range nodes {
-			fmt.Fprintf(w, "%d\t%s\n",
+			fmt.Fprintf(w, "%d\t%s\t%s\n",
 				i.ID,
 				i.Name,
+				func() string {
+					var points []point
+					if err := json.Unmarshal(i.Points, &points); err != nil {
+						panic(err)
+					}
+					arr := make([]string, len(points))
+					for i, t := range points {
+						arr[i] = t.Type
+					}
+					return strings.Join(arr, ",")
+				}(),
 			)
 		}
 
