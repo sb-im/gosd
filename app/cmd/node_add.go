39d75f9bf51112d00f666caad85289e949fc7855
a-wing <1@233.email>
2022-03-11 00:14:22 +0800

    feat(cmd): add node points filepath

--- app/cmd/node_add.go
+++ app/cmd/node_add.go
@@ -1,6 +1,10 @@
 package cmd
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+
 	"github.com/urfave/cli/v2"
 )
 
@@ -15,10 +19,21 @@ var nodeAddCmd = &cli.Command{
 		&cli.UintFlag{Name: "team"},
 		&cli.StringFlag{Name: "name"},
 	},
+	ArgsUsage: "<point path>",
 	Action: ex(func(c *exContext) error {
+		path := c.ctx.Args().First()
+		f, err := os.Open(path)
+		if err != nil {
+			panic(err)
+		}
+		data, err := ioutil.ReadAll(f)
+		if err != nil {
+			panic(err)
+		}
 		return c.cnt.NodeCreate(&map[string]interface{}{
 			"team_id": c.ctx.Uint("team"),
 			"name":    c.ctx.String("name"),
+			"points":  json.RawMessage(data),
 		})
 	}),
 }
