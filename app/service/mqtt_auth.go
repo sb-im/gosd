950e517a6512ebbf365828f378687ba1ef412eba
a-wing <1@233.email>
2022-03-16 20:43:49 +0800

    fix(service): mqtt access

--- app/service/mqtt_auth.go
+++ app/service/mqtt_auth.go
@@ -14,6 +14,14 @@ const (
 	mqttAuthACLPrefix  = "mqtt_acl:"
 )
 
+// Emqx auth acl plugin
+// access: Allowed operations: subscribe (1), publish (2), both subscribe and publish (3)
+const (
+	mqttAuthAccessSubscribe = 1
+	mqttAuthAccessPublish   = 2
+	mqttAuthAccessPubSub    = 3
+)
+
 func genToken(n int) (string, error) {
 	b := make([]byte, n)
 	if _, err := rand.Read(b); err != nil {
@@ -36,7 +44,7 @@ func (s *Service) MqttAuthACL(teamID uint, user string) string {
 
 	acl := make(map[string]interface{})
 	for _, node := range nodes {
-		acl["nodes/"+strconv.Itoa(int(node.ID))+"/#"] = true
+		acl["nodes/"+strconv.Itoa(int(node.ID))+"/#"] = mqttAuthAccessPubSub
 	}
 
 	s.rdb.HSet(context.Background(), mqttAuthACLPrefix+user, acl)
