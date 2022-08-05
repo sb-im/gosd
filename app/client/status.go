c151518b5cd510b1f89f52b701835db22c4e77da
a-wing <1@233.email>
2022-08-05 22:36:50 +0800

    test(e2e): add auth

--- app/client/status.go
+++ app/client/status.go
@@ -1,10 +1,14 @@
 package client
 
 import (
+	"errors"
 	"net/http"
 )
 
 func (c *Client) ServerStatus() error {
-	_, err := c.do(http.MethodGet, c.endpoint+"/status", nil)
+	res, err := c.do(http.MethodGet, c.endpoint+"/status", nil)
+	if res.StatusCode != http.StatusOK {
+		return errors.New("status error: " + res.Status)
+	}
 	return err
 }
---
+++ tests/integration/auth_test.go
@@ -0,0 +1,42 @@
+package integration_test
+
+import (
+	"context"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"sb.im/gosd/app/api"
+	"sb.im/gosd/app/client"
+	"sb.im/gosd/app/cmd"
+	"sb.im/gosd/app/config"
+	"sb.im/gosd/tests/help"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNoAuth(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	t.Setenv("SINGLE_USER", "true")
+	c := help.StartSingleServer(t, ctx)
+	assert.NoError(t, c.ServerStatus(), "Http Error")
+	time.Sleep(1 * time.Second)
+}
+
+func TestAuthApiKey(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	t.Setenv("SINGLE_USER", "false")
+	t.Setenv("API_KEY", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+
+	cfg := config.Parse()
+	cfg.StorageURL = "file://" + t.TempDir()
+
+	server := httptest.NewServer(cmd.NewHandler(ctx, cfg))
+	cb := client.NewClient(server.URL+api.ApiPrefix, "error_api_key")
+	assert.Error(t, cb.ServerStatus(), "Http Error")
+
+	cg := client.NewClient(server.URL+api.ApiPrefix, cfg.ApiKey)
+	assert.NoError(t, cg.ServerStatus(), "Http Error")
+}
