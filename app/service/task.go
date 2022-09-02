0b7ac0a00ba0d77f940f50ce97093b9656bdae11
a-wing <1@233.email>
2022-09-02 11:23:24 +0800

    feat(service): add traceid task id #78

--- app/service/task.go
+++ app/service/task.go
@@ -4,22 +4,18 @@ import (
 	"context"
 	"encoding/json"
 
-	log "github.com/sirupsen/logrus"
+	"sb.im/gosd/app/logger"
 	"sb.im/gosd/app/model"
 )
 
 func (s *Service) TaskRun(ctx context.Context, task *model.Task) error {
-	log.Println(task)
-
 	files := make(map[string]string)
 	extra := make(map[string]string)
 
 	json.Unmarshal(task.Files, &files)
 	json.Unmarshal(task.Extra, &extra)
 
-	log.Println("files: ", files)
-	log.Println("extra: ", extra)
-
+	logger.WithContext(ctx).Infof("%+v\t%v\t%v", task, files, extra)
 	return s.worker.AddTask(ctx, task)
 }
 
