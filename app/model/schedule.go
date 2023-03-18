21f57eb57753bf9a08f0567665bcb47a86973b2e
a-wing <1@233.email>
2023-03-18 19:09:37 +0800

    fix(model): schedule take error

--- app/model/schedule.go
+++ app/model/schedule.go
@@ -9,7 +9,7 @@ import (
 type Schedule struct {
 	Model
 	TeamID uint   `json:"-"`
-	TaskID uint   `json:"task" gorm:"not null" form:"task_id"`
+	TaskID uint   `json:"task_id" gorm:"not null" form:"task_id"`
 	Enable bool   `json:"enable" gorm:"not null;default:false" form:"enable"`
 	Name   string `json:"name" form:"name"`
 	Cron   string `json:"cron" form:"cron"`
